9253fc0e0ab8d790908d286ba6abbf6fa5470631
Prudhvi Raj Bhashampally <bprudhviraj77@gmail.com>
2026-01-16 01:43:27 -0500

    Update HelloWorldProjectApplication - disable banner mode

--- Java/Spring/SpringBoot/HelloWorld/demo/src/main/java/com/learning/demo/HelloWorldProjectApplication.java
+++ Java/Spring/SpringBoot/HelloWorld/demo/src/main/java/com/learning/demo/HelloWorldProjectApplication.java
@@ -1,5 +1,6 @@
 package com.learning.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldProjectApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(HelloWorldProjectApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.run(args);
 	}
 
 	@RequestMapping("/")
