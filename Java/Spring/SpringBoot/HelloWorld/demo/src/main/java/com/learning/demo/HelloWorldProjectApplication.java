fd48c44ade2aa6d4404a290c5ca8adaff30f6361
Prudhvi Raj Bhashampally <bprudhviraj77@gmail.com>
2026-01-21 18:45:23 -0500

    Demonstration of Application Exit

--- Java/Spring/SpringBoot/HelloWorld/demo/src/main/java/com/learning/demo/HelloWorldProjectApplication.java
+++ Java/Spring/SpringBoot/HelloWorld/demo/src/main/java/com/learning/demo/HelloWorldProjectApplication.java
@@ -1,39 +1,19 @@
 package com.learning.demo;
 
-import org.springframework.boot.Banner;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@RestController
 public class HelloWorldProjectApplication {
 
+	@Bean
+	public ExitCodeGenerator exitCodeGenerator() {
+		return () -> 42;
+	}
+
 	public static void main(String[] args) {
-		// Best for standard apps
-		SpringApplication app = new SpringApplicationBuilder(HelloWorldProjectApplication.class)
-				.bannerMode(Banner.Mode.OFF)
-				.build();
-		app.run(args);
-
-		// Best for dynamic bootstrapping
-		// SpringApplication app = new SpringApplicationBuilder().sources(HelloWorldProjectApplication.class)
-		// 		.bannerMode(Banner.Mode.OFF)
-		// 		.build();
-		// app.run(args);
-	}
-
-	@RequestMapping("/")
-	public String home() {
-		return "Home Page!";
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		System.exit(SpringApplication.exit(SpringApplication.run(HelloWorldProjectApplication.class, args)));
 	}
 }
