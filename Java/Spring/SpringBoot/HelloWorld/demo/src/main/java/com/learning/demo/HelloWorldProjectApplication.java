38f127be2607151ae6757e9da50f0d17c63caef3
Prudhvi Raj Bhashampally <bprudhviraj77@gmail.com>
2026-01-26 15:32:01 -0500

    Restful Web Service with Spring

--- Java/Spring/SpringBoot/HelloWorld/demo/src/main/java/com/learning/demo/HelloWorldProjectApplication.java
+++ Java/Spring/SpringBoot/HelloWorld/demo/src/main/java/com/learning/demo/HelloWorldProjectApplication.java
@@ -1,19 +1,11 @@
 package com.learning.demo;
 
-import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 @SpringBootApplication
 public class HelloWorldProjectApplication {
 
-	@Bean
-	public ExitCodeGenerator exitCodeGenerator() {
-		return () -> 42;
-	}
-
 	public static void main(String[] args) {
-		System.exit(SpringApplication.exit(SpringApplication.run(HelloWorldProjectApplication.class, args)));
-	}
+		SpringApplication.run(HelloWorldProjectApplication.class, args);
+	}	
 }
